942656cafd07e0e2e5829637da1d3ff5576d5d70
DangDD <dn8118545@gmail.com>
2021-04-05 23:21:02 +0700

    Signed-off-by: DangDD <dn8118545@gmail.com>

--- ĐỒ ÁN/web_phim/watch.php
+++ ĐỒ ÁN/web_phim/watch.php
@@ -7,6 +7,42 @@
 ?>
 <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v10.0" nonce="ZFvzpESY"></script>
+<style type="text/css">
+    @import url(//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css);
+/*reset css*/
+div,label{margin:0;padding:0;}
+body{margin:20px;}
+h1{font-size:1.5em;margin:10px;}
+/****** Style Star Rating Widget *****/
+#rating{border:none;float:left;}
+#rating>input{display:none;}/*ẩn input radio - vì chúng ta đã có label là GUI*/
+#rating>label:before{margin:5px;font-size:1.25em;font-family:FontAwesome;display:inline-block;content:"\f005";}/*1 ngôi sao*/
+#rating>.half:before{content:"\f089";position:absolute;}/*0.5 ngôi sao*/
+#rating>label{color:#ddd;float:right;}/*float:right để lật ngược các ngôi sao lại đúng theo thứ tự trong thực tế*/
+/*thêm màu cho sao đã chọn và các ngôi sao phía trước*/
+#rating>input:checked~label,
+#rating:not(:checked)>label:hover, 
+#rating:not(:checked)>label:hover~label{color:#FFD700;}
+/* Hover vào các sao phía trước ngôi sao đã chọn*/
+#rating>input:checked+label:hover,
+#rating>input:checked~label:hover,
+#rating>label:hover~input:checked~label,
+#rating>input:checked~label:hover~label{color:#FFED85;}
+#rating{
+    padding: 10px;
+}
+#rate-content{
+    font-size: 20px;
+}
+</style>
+<script type="text/javascript">
+    function calcRate(r) {
+ const f = ~~r,//Tương tự Math.floor(r)
+ id = 'star' + f + (r % f ? 'half' : '')
+ id && (document.getElementById(id).checked = !0)
+}
+
+</script>
 <div id="content">
     <div  id="movie-display">
         <div class="row cur-location">
@@ -49,7 +85,7 @@
             </div>
             <div class="share">
                 <div class="row">
-                    <button type="button" class="btn btn-secondary">
+                    <button type="button" class="btn btn-secondary" name="btn-secondary">
                         <img src="images/icons/plus.png" alt="Share" width="20px"> Thêm vào hộp
                     </button>
                     <button type="button" class="btn btn-primary share_fb">
@@ -63,6 +99,38 @@
                 </div>
 
             </div>
+            <div id="rating">
+    <span id="rate-content">Vote here:</span>
+    <input type="radio" id="star5" name="rating" value="5" />
+    <label class = "full" for="star5" title="Awesome - 5 stars"></label>
+    
+    <input type="radio" id="star4half" name="rating" value="4 and a half" />
+    <label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+    
+    <input type="radio" id="star4" name="rating" value="4" />
+    <label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+    
+    <input type="radio" id="star3half" name="rating" value="3 and a half" />
+    <label class="half" for="star3half" title="Meh - 3.5 stars"></label>
+    
+    <input type="radio" id="star3" name="rating" value="3" />
+    <label class = "full" for="star3" title="Meh - 3 stars"></label>
+    
+    <input type="radio" id="star2half" name="rating" value="2 and a half" />
+    <label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+    
+    <input type="radio" id="star2" name="rating" value="2" />
+    <label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+    
+    <input type="radio" id="star1half" name="rating" value="1 and a half" />
+    <label class="half" for="star1half" title="Meh - 1.5 stars"></label>
+    
+    <input type="radio" id="star1" name="rating" value="1" />
+    <label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+    
+    <input type="radio" id="starhalf" name="rating" value="half" />
+    <label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+</div>
         </div>
     </div>
     <div  id="detail">
@@ -82,7 +150,6 @@
                 </ul>
             </fieldset>
         </div>
-
     </div>
     <div  id="server-list">
         <div class="row">
@@ -99,7 +166,9 @@
                   <?php } ?>
                 </div>
             </div>
-        </div>
+        </div>           
     </div>
+<div class="comments-field">
     <div class="fb-comments" data-href="http://localhost/php_start/%C4%90%E1%BB%92%20%C3%81N/web_phim/web_phim/?mod=watch&amp;film_id=116&amp;episode=1" data-width="100%" data-numposts="5"></div>
+</div>
 </div>
